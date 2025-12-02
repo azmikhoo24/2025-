1c44666fd2b48eb0738c49dc47f5ff634371f53d
azmikhoo24 <azmikhoo24@gmail.com>
2025-12-02 18:15:52 +0800

    Initial commit

---
+++ General_notes.md
@@ -0,0 +1,12 @@
+DAY 1
+python -m venv myvenv
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+myvenv\Scripts\activate
+
+crtl + ? = #
+
+Day 2
+inside .gitignore
+.env          
+myvenv          
+__pycache__
---
+++ Python_Coding/01.py
@@ -0,0 +1,35 @@
+name = "Bob" # String
+age = 25 # Integer
+weight = 70.5 # Float
+is_female = False # Boolean
+
+print (name) # Print the variable 'name'
+print (type(name)) # Print the type of variable 'name'
+print (age) # Print the variable 'age'
+print (type(age)) # Print the type of variable 'age'
+print (weight) # Print the variable 'weight'
+print (type(weight)) # Print the type of variable 'weight'
+print (is_female) # Print the variable 'is_female'
+print (type(is_female)) # Print the type of variable 'is_female'
+
+#Operations
+x = 10
+y = 3
+print (x + y) # Addition
+print (x - y) # Subtraction
+print (x * y) # Multiplication
+print (x / y) # Division
+print (x % y) # Modulus
+print (x ** y) # Exponentiation
+print (x // y) # Floor Division
+
+#Exercise:
+# 1. Create variables that consist String, Integer, Float, and Boolean
+# data types. Print value for each key.
+# 2. Convert Celsius to Fahrenheit. (F = C * 9/5 + 32)
+# celcius = 25
+
+celcius = 25
+fahrenheit = celcius * 9/5 + 32
+print("Celsius:", celcius)
+print("Fahrenheit:", fahrenheit)
